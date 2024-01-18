d2ae4253dd9f9edd5423c6067dcc7d3a64c35c36
Alexei Kosykh <aleksei-kosykh@mail.ru>
2024-01-18 03:07:55 +0300

    Check email send

---
+++ php/send.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$message = htmlspecialchars($message);
+
+$name = urldecode($name);
+$email = urldecode($email);
+$message = urldecode($message);
+
+$name = trim($name);
+$email = trim($email);
+$message = trim($message);
+if (mail("aleksei-kosykh@mail.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: temo@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}
+?>
