cccf0b448e4da99e6586c230315986b363ebf505
Alexei Kosykh <aleksei-kosykh@mail.ru>
2024-01-23 17:09:26 +0300

    Zoom for image created

--- js/script.js
+++ js/script.js
@@ -127,6 +127,13 @@ window.onclick = function (event) {
   }
 };
 
+// doubleclick
+const stakeImg = document.querySelector('.modal__stake_img');
+
+stakeImg.addEventListener('dblclick', (event) => {
+  stakeImg.classList.toggle('zoom');
+});
+
 func(plan);
 func(one);
 func(two);
